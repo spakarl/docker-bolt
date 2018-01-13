5532111a50f327544923e9dee46fcb58b4585485
Karlheinz Spaeth <karlheinz.spaeth@gmail.com>
2018-01-13 15:12:52 +0100

    some minor changes

--- 3.4/mariadb/start-bolt.sh
+++ 3.4/mariadb/start-bolt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./vendor/bin/nut database:update -n;
+pushd /var/www/html
 mkdir -p ./app/cache;
 mkdir -p ./app/config;
 mkdir -p ./app/database;
@@ -10,8 +11,8 @@ mkdir -p ./extensions;
 chmod -R 0777 ./app;
 chmod -R 0777 ./public/extensions;
 chmod 0777 ./public/files;
-
-
 chmod -R 0777 ./extensions;
+popd
+
 rm -f /var/run/apache2/apache2.pid
 exec apache2-foreground
