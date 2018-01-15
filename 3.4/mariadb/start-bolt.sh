7fb3540b60fa260e067c5446fa21e912f9c04fbc
spakarl <karlheinz.spaeth@gmail.com>
2018-01-15 22:25:37 +0100

    unfinished changes

--- 3.4/mariadb/start-bolt.sh
+++ 3.4/mariadb/start-bolt.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+
+is_empty() {
+    test -e "$1/"* 2>/dev/null
+    case $? in;
+        1)   return 0 ;;
+        *)   return 1 ;;
+    esac
+}
+
 ./vendor/bin/nut database:update -n;
+
+
 pushd /var/www/html
-mkdir -p ./app/cache;
-mkdir -p ./app/config;
-mkdir -p ./app/database;
-mkdir -p ./public/extensions;
-mkdir -p ./public/extensions/var;
-mkdir -p ./public/files;
-mkdir -p ./extensions;
-chmod -R 0777 ./app;
-chmod -R 0777 ./public/extensions;
-chmod 0777 ./public/files;
-chmod -R 0777 ./extensions;
+mkdir -p ./app/cache
+mkdir -p ./app/config
+# mkdir -p ./app/database
+mkdir -p ./public/extensions
+mkdir -p ./public/extensions/var
+mkdir -p ./public/files
+mkdir -p ./extensions
+chown -R www-data:www-data .
+chmod -R 0777 ./app
+chmod -R 0777 ./public/extensions
+chmod 0777 ./public/files
+chmod -R 0777 ./extensions
 popd
 
 rm -f /var/run/apache2/apache2.pid
